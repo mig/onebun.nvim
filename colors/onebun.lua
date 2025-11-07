f3151a0fc5e2e3cf7d25a6b573e66754ca81200e
Matt Swasey <matt.swasey@gmail.com>
2025-11-07 13:16:22 -0500

    More explicit html/xml/jsx/tsx definitions

--- colors/onebun.lua
+++ colors/onebun.lua
@@ -6,4 +6,13 @@ end
 vim.g.colors_name = "onebun"
 vim.o.background = "dark"
 
+for _, mod in ipairs({
+	"onebun",
+	"onebun.util",
+	"onebun.palette",
+	"onebun.highlights",
+}) do
+	package.loaded[mod] = nil
+end
+
 require("onebun").load()
